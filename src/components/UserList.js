f826384e06102f25345749f78a74297f12fc6856
Farah Najib <farahnajib@mac.local>
2024-03-16 23:19:30 +0100

    few changes

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,12 +3,12 @@ import Image from 'next/image'
 const UserList = ({ users }) => {
     return (
         <>
-            <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
                 {users.map((user) => (
-                    <li key={user.id} class="pb-3 sm:pb-4">
-                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    <li key={user.id} className="pb-3 sm:pb-4">
+                        <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                            <div className="flex-1 min-w-0">
+                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
                                     <Link
                                         legacyBehavior
                                         href={`/users/${user.id}`}
@@ -16,11 +16,11 @@ const UserList = ({ users }) => {
                                         <a>{user.name}</a>
                                     </Link>
                                 </p>
-                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                                     {user.email}
                                 </p>
                             </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                            <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                 {user.username}{' '}
                             </div>
                         </div>
