e662c958b1722e195ba7449b3d163d9266a6b0ea
Farah Najib <farahnajib@mac.local>
2024-03-16 23:47:30 +0100

    refactor tests

--- src/components/UserPost.js
+++ src/components/UserPost.js
@@ -3,11 +3,9 @@ const UserPost = ({ title, body, user_id }) => {
         <>
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {title}
-                        </h5>
-                    </a>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {title}
+                    </h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {body}
                     </p>
--- src/tests/UserPost.test.js
+++ src/tests/UserPost.test.js
@@ -1,15 +1,16 @@
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import UserPost from '../components/UserPost'
 
 test('renders user post with title, body, and read more link', () => {
     const title = 'Sample Title'
     const body = 'This is a sample body text for testing.'
-    render(<UserPost title={title} body={body} />)
+    render(<UserPost title={title} body={body} user_id={1} />)
     const titleElement = screen.getByText(title)
     const bodyElement = screen.getByText(body)
-    const readMoreLink = screen.getByText('Read more')
+    const userDetailsLink = screen.getByText('User details')
 
     expect(titleElement).toBeInTheDocument()
     expect(bodyElement).toBeInTheDocument()
-    expect(readMoreLink).toBeInTheDocument()
+    expect(userDetailsLink).toBeInTheDocument()
 })
